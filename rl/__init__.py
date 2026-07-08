2a29cd99a7d296fe92860c31080ab0e30d288f9b
Krrisha Patel <krrishap@amazon.com>
2026-07-07 23:45:51 -0700

    feat: add self-play RL training with league-style opponent pool

--- rl/__init__.py
+++ rl/__init__.py
@@ -1,5 +1,6 @@
 """Reinforcement learning environment wrapping the exchange simulator."""
 
 from .trading_env import TradingEnv
+from .self_play_env import SelfPlayEnv, OpponentPolicy
 
-__all__ = ["TradingEnv"]
+__all__ = ["TradingEnv", "SelfPlayEnv", "OpponentPolicy"]
